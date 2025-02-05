33bdc89b7649596ec507fb8a782ee8e1fdade6b8
Keerthy Baby <keerthybaby025@gmail.com>
2025-02-05 16:46:06 +0530

    Data fetching

--- app/lib/models.js
+++ app/lib/models.js
@@ -74,6 +74,6 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-
-export const User =mongoose.models.User || mongoose.model("User",userSchema);
-export const Product = mongoose.model.Product || mongoose.model("Product",productSchema);
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
+export const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
--- app/lib/utils.js
+++ app/lib/utils.js
@@ -5,9 +5,10 @@ const connection = {};
 try{
   if(connection.isConnection) return;
   const db = await mongoose.connect(process.env.MONGO);
-  connection.isConnected = db.connection[0].readyState;
+  connection.isConnected = db.connections[0].readyState;
 
 } catch(error){
+  console.log(error);
   throw new Error(error);
 }
 }
