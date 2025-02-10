75476889a0e49b116d32d072f7c2a70654e3c56b
Keerthy Baby <keerthybaby025@gmail.com>
2025-02-10 11:28:41 +0530

    Next-auth protected Route with middleware

---
+++ middleware.js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./app/authconfig";
+// import { authConfig } from "./app/authconfig";
+
+export default NextAuth(authConfig).auth;
+
+// export const config = {
+//   matcher: [
+//     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+//   ],
+// };
+
+export const config = {
+  matcher: ['/((?!api|static|.*\\..*|_next).*)'],
+};
